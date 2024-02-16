9cd7a6b8546287332d62bb997a492eae995fabef
ekolesnyk <eugene.kolesnyk@customertimes.com>
2024-02-16 20:01:24 +0100

    added help and exit commands

---
+++ command_help.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func callbackHelp() error {
+	fmt.Println("Available commands:")
+	availableCommands := getCommands()
+	for _, command := range availableCommands {
+		fmt.Printf(" - %s - %s\n", command.name, command.description)
+	}
+	fmt.Println("")
+	return nil
+}
---
+++ repl.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print(">")
+		scanner.Scan()
+		text := scanner.Text()
+
+		cleaned := cleanInput(text)
+		if len(cleaned) == 0 {
+			continue
+		}
+		commandName := cleaned[0]
+
+		availableCommands := getCommands()
+		command, ok := availableCommands[commandName]
+		if !ok {
+			fmt.Println("Unknown command")
+			continue
+		}
+		command.callback()
+	}
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	commands := map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    callbackHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    exitCallback,
+		},
+	}
+	return commands
+}
+
+func cleanInput(str string) []string {
+	lowered := strings.ToLower(str)
+	words := strings.Fields(lowered)
+	return words
+}
